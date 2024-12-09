b1b879908c0ec57cb3383128691319cf1df73e21
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2024-12-09 17:57:26 +0600

    react authentic data dynamic rendering

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,33 @@
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from "../../firebase/firebase.init";
+import { useState } from "react";
 
 const Login = () => {
+    const [user, setUser] = useState(null);
+    console.log(user)
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
-    const handleGoogleSingIn = () =>{
-        signInWithPopup(auth,provider)
-        .then(result =>{
-            const user = result.user;
-            console.log(user);
-        })
-        .catch(error =>{
-            console.log('error',error.massage)
-        })
+    const handleGoogleSingIn = () => {
+        signInWithPopup(auth, provider)
+            .then(result => {
+                const loginUser = result.user;
+                console.log(loginUser);
+                setUser(loginUser)
+            })
+            .catch(error => {
+                console.log('error', error.massage)
+            })
     }
     return (
-       <div>
-        <button onClick={handleGoogleSingIn}>google login</button>
-       </div>
+        <div>
+            <button onClick={handleGoogleSingIn}>google login</button>
+            {user && <div>
+                <h2>user : {user.displayName}</h2>
+                <p>Email:{user.email} </p>
+                <img src={user.photoURL} alt="" />
+        
+            </div>}
+        </div>
     );
 };
 
