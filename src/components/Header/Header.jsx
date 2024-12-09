b6486108052d750b0e24dd71d8ea12d86226fd24
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2024-12-09 12:54:53 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h2>This is a Header</h2>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRgqJ5ZS10pjJn9dLaOObWXHmpkQuwbcg",
+  authDomain: "simple-firebase-30c7e.firebaseapp.com",
+  projectId: "simple-firebase-30c7e",
+  storageBucket: "simple-firebase-30c7e.firebasestorage.app",
+  messagingSenderId: "887278448812",
+  appId: "1:887278448812:web:c0192dd5a93c62adf42f09"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
